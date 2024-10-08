540713cc66541392a0000284856ef53fff60c8a4
onirochan <mrgintoki92@hotmail.it>
2024-10-08 02:27:59 +0200

    Added some basic CSS and updated the index

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html>
 <head>
+    <title>Skohn</title>
     <meta charset="utf-8">
-    <title>Hello, Nginx!</title>
+    <!-- Dark mode Bulma css -->
+    <!-- <link rel="stylesheet" href="./css/bulma.min.css"> -->
+    <!-- Light mode Bulma css -->
+    <link rel="stylesheet" href="./css/bulma-no-dark-mode.min.css">
 </head>
 <body>
-    <?php
-    	phpinfo();
-    ?>
+    <div class="box">
+        LANDING
+    </div>
 </body>
 </html>
 
